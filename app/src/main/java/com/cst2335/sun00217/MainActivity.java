5b890c6a4745b37988ea55f5edd2849ebb3234e3
QuinnS <sun00217@algonquinlive.com>
2024-09-26 23:57:05 -0400

    Initial commit with translations and layout

---
+++ app/src/main/java/com/cst2335/sun00217/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cst2335.sun00217;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
